7abbe8f60040afa59227a22caa8a42aa78177fd7
Syntaxerror-debug <kp565799@gmail.com>
2026-09-01 21:38:49 +0530

    feat: overhaul index and approach pages with new branding, favicon, and responsive layout improvements.

--- navbar.js
+++ navbar.js
@@ -1,87 +1,107 @@
-let navbar = document.createElement("ul");
+let navbar = document.createElement("div");
 navbar.setAttribute("id", "navbar");
 
-let menu = document.createElement("ul");
-menu.setAttribute("id", "menu")
+let navLeft = document.createElement("div");
+navLeft.className = "nav-left";
 
-let li1 = document.createElement("li");
+let navRight = document.createElement("div");
+navRight.className = "nav-right";
+
+let li1 = document.createElement("p");
 li1.innerText = "STUDIO NAMMA";
+li1.className = "nav-item logo";
+li1.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
 
-let li2 = document.createElement("li");
+let li2 = document.createElement("p");
 li2.innerText = "DARK MODE";
+li2.className = "nav-item dark-toggle";
+li2.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    li2.innerText = "LIGHT MODE";
+  } else {
+    li2.innerText = "DARK MODE";
+  }
+});
 
-
-let li3 = document.createElement("li");
+let li3 = document.createElement("p");
 li3.innerText = "MENU";
+li3.className = "nav-item menu-btn";
 
-let li4 = document.createElement("li");
-li4.innerText = "LET'S TALK";
+let li4 = document.createElement("p");
+li4.innerText = "LET'S TALK!";
+li4.className = "nav-item talk-btn";
 
 function menuEffect(element) {
-
   element.addEventListener("mouseenter", () => {
-    if (element.innerText == "MENU") {
-      element.innerText = "OPEN"
+    if (element.innerText === "MENU") {
+      element.innerText = "OPEN";
+    } else if (element.innerText === "LET'S TALK!") {
+      element.innerText = "CONTACT US";
     }
-    else if(element.innerText == "LET'S TALK"){
-      element.innerText = "CONTACT US"
-    }
-  })
+  });
 
-  element.addEventListener("mouseout", () => {
-    if (element.innerText == "OPEN") {
-      element.innerText = "MENU"
+  element.addEventListener("mouseleave", () => {
+    if (element.innerText === "OPEN") {
+      element.innerText = "MENU";
+    } else if (element.innerText === "CONTACT US") {
+      element.innerText = "LET'S TALK!";
     }
-    else if(element.innerText == "CONTACT US"){
-      element.innerText = "LET'S TALK"
-    }
-  })
+  });
 }
-
-
-li3.addEventListener("click", ()=>{
-  if(li3.innerText == "OPEN"){
-    menu.classList.add("show");
-    li3.innerText = "CLOSE"
-  }
-  else if(li3.innerText == "CLOSE"){
-    menu.classList.remove("show")
-    li3.innerText = "OPEN"
-  }
-})
 
 menuEffect(li3);
 menuEffect(li4);
 
-function createListElement(value){
+let menu = document.createElement("ul");
+menu.setAttribute("id", "menu");
+
+li3.addEventListener("click", () => {
+  if (li3.innerText === "OPEN" || li3.innerText === "MENU") {
+    menu.classList.add("show");
+    document.body.classList.add("menu-open");
+    li3.innerText = "CLOSE";
+  } else if (li3.innerText === "CLOSE") {
+    menu.classList.remove("show");
+    document.body.classList.remove("menu-open");
+    li3.innerText = "OPEN";
+  }
+});
+
+let validIndex = 0;
+
+function createListElement(value) {
   let li = document.createElement("li");
-  li.innerText = value;
+  if (value !== "") {
+    let wrapper = document.createElement("div");
+    wrapper.className = "menu-item-wrapper";
+    let span = document.createElement("span");
+    span.className = "menu-item-text";
+    span.innerText = value;
+    span.style.transitionDelay = `${0.2 + validIndex * 0.06}s`;
+    validIndex++;
+    wrapper.append(span);
+    li.append(wrapper);
+
+    li.addEventListener("click", () => {
+      menu.classList.remove("show");
+      document.body.classList.remove("menu-open");
+      if (li3.innerText === "CLOSE") {
+        li3.innerText = "MENU";
+      }
+      if (value === "HOME") {
+        window.location.href = "index.html";
+      } else if (value === "APPROACH") {
+        window.location.href = "approach.html";
+      } else {
+        alert(value);
+      }
+    });
+  }
   menu.append(li);
-
-  li.addEventListener("click",()=>{
-    if(li.innerText == "HOME"){
-      window.location.href = "/home/index.html";
-    }else if(li.innerText == "WORK"){
-      alert("Work");
-    }
-    else if(li.innerText == "STUDIO"){
-      alert("STUDIO");
-    }
-    else if(li.innerText == "SERVICES"){
-      alert("SERVICES");
-    }
-    else if(li.innerText == "PLANS"){
-      alert("PLANS");
-    }
-    else if(li.innerText == "APPROACH"){
-      window.location.href = "/approach/approach.html";
-    }
-    else if(li.innerText == "NEWS"){
-      alert("NEWS");
-    }
-  })
 }
-
 
 createListElement("HOME");
 createListElement("");
@@ -92,8 +112,49 @@ createListElement("PLANS");
 createListElement("APPROACH");
 createListElement("NEWS");
 
-console.log(menu);
 
-navbar.append(li1, li2, li3, li4);
-document.body.append(navbar, menu);
+navLeft.append(li1, li2);
+navRight.append(li3, li4);
+navbar.append(navLeft, navRight);
+
+let footerBar = document.createElement("div");
+footerBar.setAttribute("id", "footer-bar");
+
+let footerLeft = document.createElement("div");
+footerLeft.className = "footer-left";
+footerLeft.innerText = "WE CRAFT BOLD DESIGN & CLEAN WEBFLOW.";
+
+let footerRight = document.createElement("div");
+footerRight.className = "footer-right";
+
+let locSpan = document.createElement("span");
+locSpan.innerText = "BARCELONA, SPAIN  ";
+
+let clockSpan = document.createElement("span");
+clockSpan.id = "live-clock";
+
+footerRight.append(locSpan, clockSpan);
+footerBar.append(footerLeft, footerRight);
+
+document.body.append(navbar, menu, footerBar);
+
+function updateClock() {
+  const clockEl = document.getElementById("live-clock");
+  if (clockEl) {
+    const now = new Date();
+    const timeStr = new Intl.DateTimeFormat('en-US', {
+      timeZone: 'Europe/Madrid',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    }).format(now);
+    clockEl.innerText = timeStr;
+  }
+}
+setInterval(updateClock, 1000);
+updateClock();
+
+
+
 
